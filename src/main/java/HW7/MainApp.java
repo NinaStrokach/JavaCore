9530322810320b7acacb1ec71d346cd673bf5980
Nina Strokach <86816744+NinaStrokach@users.noreply.github.com>
2022-06-30 13:26:29 +0400

    Delete MainApp.java

--- src/main/java/HW7/MainApp.java
+++
@@ -1,9 +0,0 @@
-package HW7;
-
-public class MainApp {
-
-    public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface();
-        userInterface.runApplication();
-    }
-}
